d65fe4f058b853542d9570e9843f9c53f78d2229
valent1n <valent1n.bazylev@gmail.com>
2022-05-23 18:34:25 +0300

    feat: 03 solved

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,19 @@
-const fs = require('fs');
 const path = require('path');
-const { readdir, stat } = fs;
-const fileDir = path.join(__dirname, 'secret-folder');
+const fs = require('fs');
+const folderPath = path.join(__dirname, 'secret-folder');
 
-readdir(fileDir,
-  { withFileTypes: true },
-  (err, files) => {
-    if (err)
-      console.log(err);
-    else {
-      files.forEach(file => {
-        if (file.isFile()){
-          const fileName = file.name.substring(0, file.name.lastIndexOf('.'));
-          const fileExt = path.extname(file.name).substring(1);
-          stat(path.join(fileDir, file.name), (err,stats) => {
-            if (err) {
-              console.log(err);
-            } else {
-              const fileSize = (stats.size / 1024).toFixed(3) + 'kb';
-              console.log(fileName,'.',fileExt,'-', fileSize);
-              return stats.size;
-            }
-          });
-        }
+fs.readdir(folderPath, { withFileTypes: true }, (err, el) => {
+  el.forEach((file) => {
+      const filePath = path.join(folderPath, file.name);
+      const File = path.parse(filePath);
+      fs.stat(filePath, (err, stats) => {
+        if (err) throw err;
+        console.log(
+          `${File.name} - ${File.ext.slice(1)} - ${stats.size/1024} kb`
+        );
       });
-    }
-  }); 
+    
+  });
+});
+
+
