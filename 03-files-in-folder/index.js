ac179f2a53eb394ea69021eec4d330ca29962add
valent1n <valent1n.bazylev@gmail.com>
2022-05-23 19:25:25 +0300

    feat:fix 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,19 @@
-const path = require('path');
-const fs = require('fs');
-const folderPath = path.join(__dirname, 'secret-folder');
+const path = require('path') 
+const fs = require('fs') 
+const folderPath = path.join(__dirname, 'secret-folder') 
 
 fs.readdir(folderPath, { withFileTypes: true }, (err, el) => {
   el.forEach((file) => {
-      const filePath = path.join(folderPath, file.name);
-      const File = path.parse(filePath);
+      const filePath = path.join(folderPath, file.name) 
+      const File = path.parse(filePath) 
       fs.stat(filePath, (err, stats) => {
-        if (err) throw err;
+        if (err) throw err 
         console.log(
           `${File.name} - ${File.ext.slice(1)} - ${stats.size/1024} kb`
-        );
-      });
+        ) 
+      }) 
     
-  });
-});
+  }) 
+}) 
 
 
